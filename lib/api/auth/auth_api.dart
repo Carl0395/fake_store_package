ca3c88f2cdea36d9aa68ce9532044ced62c7a316
Carlos <carlos@wasi.pro>
2025-05-14 14:57:13 -0500

    create user

--- lib/api/auth/auth_api.dart
+++ lib/api/auth/auth_api.dart
@@ -26,4 +26,26 @@ class AuthApi {
       }
     });
   }
+
+  static Future<Either<Failure, UserModel>> signUp({
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    final res = await HttpHelper.post(Routes.users, {
+      "username": username,
+      "email": email,
+      "password": password,
+    });
+
+    return res.fold((failure) => Left(failure), (data) {
+      try {
+        final dynamic jsonData = json.decode(data.body);
+        final UserModel user = UserModel.fromJson(jsonData);
+        return Right(user);
+      } catch (e) {
+        return Left(ParsingFailure(e.toString()));
+      }
+    });
+  }
 }
